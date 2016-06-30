86f09369568076762f046480161e3171016d9735
Sophie Gill <sophie-gill@hotmail.com>
2016-06-30 12:21:39 +0100

    completed making fizzbuzz a class method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,13 @@
 class Fixnum
-
   def fizzbuzz
-    if self == 3
+    if self % 3 == 0 && self % 5 == 0
+      "Fizzbuzz"
+    elsif self % 3 == 0
       "Fizz"
+    elsif self % 5 == 0
+      "Buzz"
+    else
+      self  
     end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
 require 'fizzbuzz'
 
 describe 'Fizzbuzz' do
-  it "returns 'Fizz' for the number 3" do
-    expect(3.fizzbuzz).to eq 'Fizz'
+  it "returns 'Fizz' if the number is divisible by 3" do
+    expect(9.fizzbuzz).to eq 'Fizz'
+    end
+  it "returns 'Buzz' if the number is divisible by 5" do
+    expect(25.fizzbuzz).to eq 'Buzz'
+  end
+  it "returns 'Fizzbuzz' for numbers that are multiples of both 3 and 5" do
+    expect(30.fizzbuzz).to eq 'Fizzbuzz'
+  end
+  it "returns the number in all other cases" do
+    expect(47.fizzbuzz).to eq 47
   end
 end
